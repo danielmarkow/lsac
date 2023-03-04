a01e644eb8ee198970ed8f6a712726f2807abc8d
Daniel Markow <daniel@Daniels-MacBook-Air.local>
2023-03-04 23:08:13 +0100

    mapping the links and comments

--- src/components/ListCommentLink.tsx
+++ src/components/ListCommentLink.tsx
@@ -1,7 +1,7 @@
 import { api } from "~/utils/api";
 import DarkButton from "./common/DarkButton";
 
-const LIMIT = 2;
+const LIMIT = 10;
 
 export default function ListCommentLink() {
   const {
@@ -16,16 +16,34 @@ export default function ListCommentLink() {
     }
   );
 
+  const flatCommentsLinks =
+    commentsLinks?.pages.flatMap((page) => page.linkComments) ?? [];
+
   return (
     <>
       <div className="mt-1 border-2 border-dashed border-gray-200 p-1">
-        {JSON.stringify(commentsLinks)}
+        <ul role="list" className="divide-y divide-gray-200">
+          {flatCommentsLinks.map((cl) => (
+            <li
+              key={cl.id}
+              className="relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50"
+            >
+              <a href={cl.link}>{cl.link}</a>
+              <div className="mt-1">
+                <p className="line-clamp-2 text-sm text-gray-600">
+                  {cl.comment}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
         <DarkButton
           onClick={() => {
             if (hasNextPage && !isFetching) {
               fetchNextPage();
             }
           }}
+          className="mt-2"
         >
           Fetch More!
         </DarkButton>
