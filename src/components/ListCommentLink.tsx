f169cc61143eb2f40f7ffc7cd3b9385e556ef633
Daniel Markow <daniel@Daniels-MacBook-Air.local>
2023-03-06 13:18:31 +0100

    open links in new tab

--- src/components/ListCommentLink.tsx
+++ src/components/ListCommentLink.tsx
@@ -28,7 +28,9 @@ export default function ListCommentLink() {
               key={cl.id}
               className="relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50"
             >
-              <a href={cl.link}>{cl.link}</a>
+              <a href={cl.link} target="_blank">
+                {cl.link}
+              </a>
               <div className="mt-1">
                 <p className="line-clamp-2 text-sm text-gray-600">
                   {cl.comment}
