47dfa8704242ea7ab139daaf56414b33ca0b41e1
Daniel Markow <daniel@Daniels-MacBook-Air.local>
2023-03-06 19:15:14 +0100

    loading spinner, landing page based on auth status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,29 +4,32 @@ import DarkButton from "~/components/common/DarkButton";
 import { signIn, useSession } from "next-auth/react";
 import CreateCommentLink from "~/components/CreateCommentLink";
 import ListCommentLink from "~/components/ListCommentLink";
-
-// import { api } from "~/utils/api";
+import Loading from "~/components/common/Loading";
 
 const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
+  const { status: sessionStatus } = useSession();
 
   return (
     <>
       <main>
-        {!sessionData ? (
+        {sessionStatus === "unauthenticated" ? (
           <div className="flex min-h-screen flex-col items-center justify-center">
             <h1>LSAC - Link Saver and Commenter</h1>
             <DarkButton className="mt-5" onClick={() => void signIn()}>
               Login
             </DarkButton>
           </div>
-        ) : (
+        ) : sessionStatus === "authenticated" ? (
           <>
             <div className="container w-screen">
               <CreateCommentLink />
               <ListCommentLink />
             </div>
           </>
+        ) : (
+          <div className="flex min-h-screen flex-col items-center justify-center">
+            <Loading />
+          </div>
         )}
       </main>
     </>
